cb393cc313f18901e303446a3c23abe904d64111
Roberto Natera <120019366+Naterar@users.noreply.github.com>
2023-11-10 11:20:26 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from replit import clear
-from art import logo
+import random
 
-print(logo)
+def get_computer_choice():
+    choices = ["rock", "paper", "scissors"]
+    return random.choice(choices)
 
-bids = {}
+def get_user_choice():
+    choice = input("Enter rock, paper, or scissors: ").lower()
+    while choice not in ["rock", "paper", "scissors"]:
+        choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
+    return choice
 
-def find_highest_bidder(bidding_record):
-  highest_bid = max(bidding_record.values())
-  winner = max(bidding_record, key=bidding_record.get)
-  print(f"The winner is {winner} with a bid of ${highest_bid}")
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "paper" and computer_choice == "rock") or \
+         (user_choice == "scissors" and computer_choice == "paper"):
+        return "You win!"
+    else:
+        return "You lose!"
 
-while True:
-  name = input("What is your name? ")
-  price = int(input("What is your bid? "))
-  bids[name] = price
-  should_continue = input("Are there any other bidders? Type 'yes' or 'no'. ")
-  if should_continue == "no":
-    break
-  clear()
+def play_game():
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    print(f"\nYou chose {user_choice}, computer chose {computer_choice}.")
+    print(determine_winner(user_choice, computer_choice))
 
-find_highest_bidder(bids)
+play_game()
