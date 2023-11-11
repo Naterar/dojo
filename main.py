4e82262fb24ccbf5d9509dd908a2c83b61c83a46
Roberto Natera <120019366+Naterar@users.noreply.github.com>
2023-11-11 17:19:06 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import random
+import string
 
-def get_computer_choice():
-    choices = ["rock", "paper", "scissors"]
-    return random.choice(choices)
+def generate_password(length):
+    if length < 6:
+        raise ValueError(f"Your password is too short. Pick a password above 6 characters. You only picked {length}.")
 
-def get_user_choice():
-    choice = input("Enter rock, paper, or scissors: ").lower()
-    while choice not in ["rock", "paper", "scissors"]:
-        choice = input("Invalid choice. Please enter rock, paper, or scissors: ").lower()
-    return choice
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    numbers = string.digits
+    symbols = string.punctuation
 
-def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
-        return "It's a tie!"
-    elif (user_choice == "rock" and computer_choice == "scissors") or \
-         (user_choice == "paper" and computer_choice == "rock") or \
-         (user_choice == "scissors" and computer_choice == "paper"):
-        return "You win!"
-    else:
-        return "You lose!"
+    password = [
+        random.choice(lowercase),
+        random.choice(uppercase),
+        random.choice(numbers),
+        random.choice(symbols)
+    ]
 
-def play_game():
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
-    print(f"\nYou chose {user_choice}, computer chose {computer_choice}.")
-    print(determine_winner(user_choice, computer_choice))
+    complete_password = lowercase + uppercase + numbers + symbols
+    password += random.choices(complete_password, k=length - 4)  
+    random.shuffle(password)
+    return ''.join(password)
 
-play_game()
+print("Welcome to password generator!")
+
+while True:
+    try:
+        password_length = int(input("How long do you want your password to be? "))
+        password = generate_password(password_length)
+        print(f"Generated Password: {password}")
+        break  
+    except ValueError as e:
+        print(e)  
