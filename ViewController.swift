92a99418470838edb1119a22712bee74d4d03259
Roberto Natera <120019366+Naterar@users.noreply.github.com>
2023-12-10 05:41:57 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iAmRich
+//
+//  Created by Roberto Natera on 12/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
