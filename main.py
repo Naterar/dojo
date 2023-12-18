6961f74a9d2d364e7a6e62efe254b93805536a19
Roberto Natera <120019366+Naterar@users.noreply.github.com>
2023-12-17 19:09:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,66 +1,25 @@
-import random
-from hangman_art import logo, stages
-from hangman_words import word_list
-from replit import clear
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def choose_word(word_list):
-  """Randomly selects a word from the word list."""
-  return random.choice(word_list)
+def caesar_cipher(text, shift_amount, direction):
+    result_text = ""
+    if direction == "decrypt":
+        shift_amount = -shift_amount
 
-def initialize_display(word_length):
-  """Creates the initial display with blanks for the chosen word."""
-  return ["_" for _ in range(word_length)]
+    for letter in text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = (position + shift_amount) % len(alphabet)
+            result_text += alphabet[new_position]
+        else:
+            result_text += letter
+    return result_text
 
-def update_display(chosen_word, display, guess):
-  """Updates the display with the guessed letter if it is in the chosen word."""
-  for position, letter in enumerate(chosen_word):
-      if letter == guess:
-          display[position] = letter
-  return display
+direction = input("Type 'encrypt' to encrypt, type 'decrypt' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-def get_user_guess():
-  """Prompts the user to guess a letter and returns it."""
-  return input("Guess a letter: ").lower()
-
-def display_status(display):
-  """Displays the current status of the word to the player."""
-  print(f"{' '.join(display)}")
-
-def hangman_game():
-  """Main function to run the Hangman game."""
-  print(logo)
-  chosen_word = choose_word(word_list)
-  word_length = len(chosen_word)
-  display = initialize_display(word_length)
-  lives = 6
-  end_of_game = False
-
-  print(f'Pssst, the solution is {chosen_word}.')
-
-  while not end_of_game:
-      guess = get_user_guess()
-
-      if guess in display:
-          print(f"You've already guessed {guess}.")
-          continue
-
-      display = update_display(chosen_word, display, guess)
-
-      if guess not in chosen_word:
-          print(f"You guessed {guess}, that's not in the word. You lose a life.")
-          lives -= 1
-          if lives == 0:
-              end_of_game = True
-              print("You lose.")
-              break
-
-      display_status(display)
-
-      if "_" not in display:
-          end_of_game = True
-          print("You win.")
-
-      print(stages[lives])
-
-if __name__ == "__main__":
-  hangman_game()
+if direction in ['encrypt', 'decrypt']:
+    processed_text = caesar_cipher(text, shift, direction)
+    print(f"Your {direction}ed text is: {processed_text}")
+else:
+    print("Invalid command.")
